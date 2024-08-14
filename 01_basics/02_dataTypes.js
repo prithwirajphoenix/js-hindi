42556ec3dc1bbea6b441553bb0f109c0fe7f142f
Prithwiraj Das <prithwirajdas004@gmail.com>
2024-08-14 06:19:27 +0000

    DATA TYPES

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; 
+// treat all JS code as newer version
+// alert(3+3) we are using node.js and not browser
+ console.log(3+3)
+
+let name = "Ahmed"
+let age = 30
+let isLoggedIn = true
+
+
+
+// number 2 ^53
+// bigint
+///string  ""
+//boolean true or false
+//null => it is a standalone value in javascript
+//undefined => value is not assigned till now
+//symbol => unique value
+//object => key value pair
+
+
+console.log(typeof "Prithwiraj")
+//null is an object but undefined is undefined
+console.log(typeof null)
+console.log(typeof undefined)
